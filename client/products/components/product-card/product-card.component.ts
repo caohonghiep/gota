750806e21ac6367ea1c0fce66f091f8ae66a032e
caohonghiep <honghiepcao@gmail.com>
2016-10-17 08:16:43 +0700

    impl product card

--- client/products/components/product-card/product-card.component.ts
+++ client/products/components/product-card/product-card.component.ts
@@ -1,15 +1,17 @@
-import { Component} from '@angular/core';
+import { Component, Input} from '@angular/core';
 import {RenderingUIAbstractComponent} from "../../../main/components/rendering-iu/rendering-ui.abstract.component";
 
 @Component({
-    selector:'gota-sidebar',
+    selector:'gota-product-card',
     moduleId :  module.id ? module.id : __moduleName,
     templateUrl: './product-card.component.html',
     styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent extends RenderingUIAbstractComponent {
     renderUI() {
-
     }
 
+    @Input() name:string;
+    @Input() price:number;
+    @Input() imageURL:string;
 }
